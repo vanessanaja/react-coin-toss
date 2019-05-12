f42c0dadc1470fc6d43d9745430e972ac4edc2b1
vanessanaja <vanessacnaja@gmail.com>
2019-05-12 12:50:39 -0700

    display correct Coin and correct stats

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
 import CoinContainer from './CoinContainer';
+import './App.css';
 
 class App extends Component {
   render() {
--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Coin from './Coin';
 import { choice } from './helpers';
 
 class CoinContainer extends Component {
   static defaultProps = {
     coins: [
-      {side: 'heads', imgSrc: "https://tinyurl.com/react-coin-heads-jpg" }, 
-      {side: 'tails', imgSrc: "https://tinyurl.com/react-coin-tails-jpg"}
+      { side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
+      { side: "tails", imgSrc: "https://tinyurl.com/react-coin-tails-jpg" }
     ]
   }
   constructor(props){
@@ -22,7 +23,6 @@ class CoinContainer extends Component {
     const newCoin = choice(this.props.coins);
     this.setState(st => {
       return{
-        ...st,
         currCoin: newCoin,
         numFlips: st.numFlips + 1,
         numHeads: st.numHeads + (newCoin.side === 'heads' ? 1 : 0),
@@ -35,12 +35,16 @@ class CoinContainer extends Component {
   }
   render(){
     return (
-      <div className="CoinContainer">
-        <h2>Flip the Coin!</h2>
-        <button onClick={this.handleClick}>Flip Coin</button>
-        <p>Out of {this.state.numFlips} flips there have been {this.state.numHeads} heads and {this.state.numTails} tails.</p>
+      <div className='CoinContainer'>
+        <h2>Let's Flip A Coin!</h2>
+        {this.state.currCoin && <Coin info={this.state.currCoin} />}
+        <button onClick={this.handleClick}>Flip Me!</button>
+        <p>
+          Out of {this.state.numFlips} flips, there have been {this.state.numHeads}{" "}
+          heads and {this.state.numTails} tails.
+        </p>
       </div>
-    )
+    );
   }
 }
 
