8e2781e14deedb0b5c9b5919b0739c05cebaa6e6
vanessanaja <vanessacnaja@gmail.com>
2019-05-12 11:38:15 -0700

    set up CoinContainer Component

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -1,9 +1,21 @@
 import React, { Container } from 'react';
 
 class CoinContainer extends Component {
+  static defaultProps = {
+    coins: [
+      {side: 'heads', imgSrc: "https://tinyurl.com/react-coin-heads-jpg" }, 
+      {side: 'tails', imgSrc: "https://tinyurl.com/react-coin-tails-jpg"}
+    ]
+  }
   constructor(props){
     super(props);
-
+    this.state = {
+      currentCoint: null,
+      numFlips: 0,
+      numHeads: 0,
+      numTails: 0
+    }
+    
   }
 }
 
